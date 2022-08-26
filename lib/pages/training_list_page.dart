3669624bc068837c8c9e2f5cf37a400c3eb1d29d
Jan Morozov <morozovjan@gmail.com>
2022-08-26 16:54:01 +0500

    Create list workouts page

---
+++ lib/pages/training_list_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WorkoutListPage extends StatelessWidget {
+  const WorkoutListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('here is the lists of workouts'),
+    );
+  }
+}
